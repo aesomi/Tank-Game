bd11d48c4d28aa791713dad14ee0c90148c7d771
ESRA AKŞİT <90726297+aesomi@users.noreply.github.com>
2022-05-23 10:23:57 +0300

    Tank Game

---
+++ TankGame/Assets/Scripts/Ai/AIBehaviour.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public abstract class AIBehaviour : MonoBehaviour
+{
+    public abstract void PerformAction(TankController tank, AIDetector detector);
+    
+}
---
+++ TankGame/Assets/Scripts/DestroyOnAudio.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnAudio : MonoBehaviour
+{
+
+    AudioSource source;
+
+    private void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(WaitCoroutine());
+    }
+
+    IEnumerator WaitCoroutine()
+    {
+        yield return new WaitForSeconds(source.clip.length);
+        Destroy(gameObject);
+    }
+
+
+
+}
